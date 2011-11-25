4bda2368368373e9afb6fbe9210e81789d70d1fd
Joe McGouran <joe_mcgouran@yahoo.ie>
2011-11-25 12:43:37 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileQuotesApp::Application.config.secret_token = '9c9e4a317c17cb6f296158b150dcc3778ba9011617ce02c9b60998898cd662303901f612760b89542a9a4f2e23ac22e4428f136a1fdd539b4d9472801276c3df'
